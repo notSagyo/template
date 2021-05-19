0c27f20b2ea109d462be7990b0ce9471aa0998b8
unknown <notsagyo@gmail.com>
2021-05-18 23:47:47 -0300

    node-sass replaced with dart-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,57 @@
-//Packages ----------------------------------------------------------------- //
+//Gulp packages ------------------------------------------------------------ //
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-dart-sass');
 var postcss = require('gulp-postcss');
+var rename = require('gulp-rename')
 //PostCSS packages
 var cssnano = require('cssnano');
 var autoprefixer = require('autoprefixer');
 
+//Variables ---------------------------------------------------------------- //
 //Processors used
-var processors = [
-    cssnano,
-    autoprefixer
-];
+var processors = [ autoprefixer ];
+var distProcessors = processors.concat([cssnano]);
+
+//URLs
+var srcPath = 'src/**';
+var destPath = 'public/css';
 
 //Tasks -------------------------------------------------------------------- //
-gulp.task('compileCSS', function() {
-    return gulp.src(['src/*.css'])
+gulp.task('buildCSS', function() {
+    return gulp.src([srcPath + '/*.css'])
         .pipe(postcss(processors))
-        .pipe(gulp.dest('public/css'));
+        .pipe(rename({dirname:''}))
+        .pipe(gulp.dest(destPath));
 })
 
-gulp.task('compileSASS', function() {
-    return gulp.src(['src/**/*.scss'])
+gulp.task('buildCSSDist', function() {
+    return gulp.src([srcPath + '/*.css'])
+        .pipe(postcss(distProcessors))
+        .pipe(rename({dirname:''}))
+        .pipe(gulp.dest(destPath));
+})
+
+gulp.task('buildSASS', function() {
+    return gulp.src([srcPath + '/*.scss'])
         .pipe(sass())
         .pipe(postcss(processors))
-        .pipe(gulp.dest('public/css'))
+        .pipe(rename({dirname:''}))
+        .pipe(gulp.dest(destPath))
+})
+
+gulp.task('buildSASSDist', function() {
+    return gulp.src([srcPath + '/*.scss'])
+        .pipe(sass())
+        .pipe(postcss(distProcessors))
+        .pipe(rename({dirname:''}))
+        .pipe(gulp.dest(destPath))
 })
 
 //Watch -------------------------------------------------------------------- //
 gulp.task('watchCSS', function(){
-    gulp.watch('**/*.css', gulp.series(['compile']))
+    gulp.watch('**/*.css', gulp.series(['buildCSS']))
 })
 
 gulp.task('watchSASS', function(){
-    gulp.watch('**/*.scss', gulp.series(['styles']))
+    gulp.watch('**/*.scss', gulp.series(['buildSASS']))
 })
