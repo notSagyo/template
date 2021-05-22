91baee402602b2866e2dc950cb9ed83e6cd4059f
unknown <notsagyo@gmail.com>
2021-05-21 22:36:06 -0300

    added rucksack

--- gulpfile.js
+++ gulpfile.js
@@ -7,54 +7,59 @@ var rename = require('gulp-rename')
 var cssnano = require('cssnano');
 var presetEnv = require('postcss-preset-env');
 var lost = require('lost');
+var rucksack = require('rucksack-css');
 
 //Variables ---------------------------------------------------------------- //
-//Processors used
-var processors = [ lost,
+//Processors
+var processors = [
+	lost(),
+	rucksack(),
 	presetEnv()
 ];
-var distProcessors = processors.concat([cssnano]);
+var distProcessors = processors.concat([(cssnano())]);
 
 //URLs
-var srcPath = 'src/**';
-var destPath = 'public/css';
+var paths = {
+	src: 'src/**',
+	dest: 'public/css'
+}
 
 //Tasks -------------------------------------------------------------------- //
 gulp.task('buildCSS', function() {
     return gulp.src([srcPath + '/*.css'])
         .pipe(postcss(processors))
         .pipe(rename({dirname:''}))
-        .pipe(gulp.dest(destPath));
+        .pipe(gulp.dest(paths.src));
 })
 
 gulp.task('buildCSSDist', function() {
     return gulp.src([srcPath + '/*.css'])
         .pipe(postcss(distProcessors))
         .pipe(rename({dirname:''}))
-        .pipe(gulp.dest(destPath));
+        .pipe(gulp.dest(paths.dest));
 })
 
 gulp.task('buildSASS', function() {
-    return gulp.src([srcPath + '/*.scss'])
+    return gulp.src([paths.src + '/*.scss'])
         .pipe(sass())
         .pipe(postcss(processors))
         .pipe(rename({dirname:''}))
-        .pipe(gulp.dest(destPath))
+        .pipe(gulp.dest(paths.dest))
 })
 
 gulp.task('buildSASSDist', function() {
-    return gulp.src([srcPath + '/*.scss'])
+    return gulp.src([paths.src + '/*.scss'])
         .pipe(sass())
         .pipe(postcss(distProcessors))
         .pipe(rename({dirname:''}))
-        .pipe(gulp.dest(destPath))
+        .pipe(gulp.dest(paths.dest))
 })
 
 //Watch -------------------------------------------------------------------- //
 gulp.task('watchCSS', function(){
-    gulp.watch('**/*.css', gulp.series(['buildCSS']))
+    gulp.watch(paths.src + '/*.css', gulp.series(['buildCSS']))
 })
 
 gulp.task('watchSASS', function(){
-    gulp.watch('**/*.scss', gulp.series(['buildSASS']))
+    gulp.watch(paths.src + '/*.scss', gulp.series(['buildSASS']))
 })
